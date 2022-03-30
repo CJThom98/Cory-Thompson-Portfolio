0149002b48ec4f4abc97a7b74623248984acdc27
Cory Thompson <cory.thompson98@gmail.com>
2022-03-30 15:15:18 -0500

    adding the projects to the projects component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
 import photo from "../../assets/img/band_browser.png";
+import photo2 from "../../assets/img/pw_gen.png";
+import photo3 from "../../assets/img/PNP.png";
 
 function Projects() {
-    const currentCategory = {
-        name: "Front End",
-        description:
-            "Technology: HTML, CSS, Javascript"
-    };
 
     return (
-        <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.description}</p>
-            <div className="flex-row">
-                <a href='https://cjthom98.github.io/band-browser/'>
-                    <img 
-                        src={photo}
-                        alt="Fron End Image"
-                        className="img-thumbnail mx-1"
-                    />
-                </a>
+        <section className="">
+            <h1>Projects</h1>
+            <br />
+            <div className=''>
+                <h3>Band Browser</h3>
+                <p>Technology: HTML, CSS, Javascript</p>
+                <div className="flex-row">
+                    <a href='https://cjthom98.github.io/band-browser/'>
+                        <img 
+                            src={photo}
+                            alt="Front End Image"
+                            className="img-thumbnail mx-1"
+                        />
+                    </a>
+                </div>
+            </div>
+            <br />
+            <div>
+                <h3>Password Generator</h3>
+                <p>Technology: Javascript</p>
+                <div className="flex-row">
+                    <a href="https://cjthom98.github.io/password_generator/">
+                        <img 
+                            src={photo2}
+                            alt="PW Javascript Image"
+                            className='img-thumbnail mx-1'
+                        />
+                    </a>
+                </div>
+            </div>
+            <div>
+                <h3>Full Stack</h3>
+                <p>Technology: HTML, CSS, Javascript, MySQL, Express.js, Node.js</p>
+                <div className="flex-row">
+                    <a href="https://cjthom98.github.io/pawnee_parks_and_rec/">
+                        <img 
+                            src={photo3}
+                            alt="Parks and Rec Image"
+                            className="img-thumbnail mx-1"
+                        />
+                    </a>
+                </div>
+            </div>
+            <div>
+                <h3>Budget Tracker</h3>
             </div>
         </section>
     );
